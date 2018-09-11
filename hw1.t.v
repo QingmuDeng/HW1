82fb457d05252a662dd4a4df44410353dfbae67d
QingmuDeng <qingmu.deng@students.olin.edu>
2018-09-11 13:27:22 -0400

    complete and submission

--- hw1.t.v
+++ hw1.t.v
@@ -3,19 +3,72 @@
 module demorgan_test ();
 
     reg A, B;
-    wire nA, nB, nAandnB;
+    /* for ~A~B */
+    // wire nA, nB, nAandnB;
 
-    demorgan dut(A, B, nA, nB, nAandnB);
+    // demorgan dut(A, B, nA, nB, nAandnB);
+
+    // initial begin
+    //     $display("A B | ~A ~B | ~A~B");     // Header for the truth table for nAandnB
+    //     A = 0; B = 0; #1
+    //     $display("%b %b |  %b  %b |    %b", A, B, nA, nB, nAandnB);
+    //     A = 0; B = 1; #1
+    //     $display("%b %b |  %b  %b |    %b", A, B, nA, nB, nAandnB);
+    //     A = 1; B = 0; #1
+    //     $display("%b %b |  %b  %b |    %b", A, B, nA, nB, nAandnB);
+    //     A = 1; B = 1; #1
+    //     $display("%b %b |  %b  %b |    %b", A, B, nA, nB, nAandnB);
+    // end
+
+    /* for ~(A+B) */
+    // wire AorB, notAorB;
+
+    // demorgan dut(A, B, AorB, notAorB);
+
+    // initial begin
+    //     $display("A B | A+B | ~(A+B)");     // Header for the truth table for nAandnB
+    //     A = 0; B = 0; #1
+    //     $display("%b %b |  %b  |     %b", A, B, AorB, notAorB);
+    //     A = 0; B = 1; #1
+    //     $display("%b %b |  %b  |     %b", A, B, AorB, notAorB);
+    //     A = 1; B = 0; #1
+    //     $display("%b %b |  %b  |     %b", A, B, AorB, notAorB);
+    //     A = 1; B = 1; #1
+    //     $display("%b %b |  %b  |     %b", A, B, AorB, notAorB);
+    // end
+
+    /* for ~(A+B) */
+    // wire AandB, notAandB;
+
+    // demorgan dut(A, B, AandB, notAandB);
+
+    // initial begin
+    //     $display("A B | A&B | ~(A&B)");     // Header for the truth table for nAandnB
+    //     A = 0; B = 0; #1
+    //     $display("%b %b |  %b  |     %b", A, B, AandB, notAandB);
+    //     A = 0; B = 1; #1
+    //     $display("%b %b |  %b  |     %b", A, B, AandB, notAandB);
+    //     A = 1; B = 0; #1
+    //     $display("%b %b |  %b  |     %b", A, B, AandB, notAandB);
+    //     A = 1; B = 1; #1
+    //     $display("%b %b |  %b  |     %b", A, B, AandB, notAandB);
+    // end
+
+    /* for ~A+~B */
+    wire nA, nB, nAornB;
+
+    demorgan dut(A, B, nA, nB, nAornB);
 
     initial begin
-        $display("A B | ~A ~B | ~A~B");     // Header for the truth table for nAandnB
+        $display("A B | ~A ~B | ~A+~B");     // Header for the truth table for nAandnB
         A = 0; B = 0; #1
-        $display("%b %b |  %b  %b |    %b", A, B, nA, nB, nAandnB);
+        $display("%b %b |  %b  %b |    %b", A, B, nA, nB, nAornB);
         A = 0; B = 1; #1
-        $display("%b %b |  %b  %b |    %b", A, B, nA, nB, nAandnB);
+        $display("%b %b |  %b  %b |    %b", A, B, nA, nB, nAornB);
         A = 1; B = 0; #1
-        $display("%b %b |  %b  %b |    %b", A, B, nA, nB, nAandnB);
+        $display("%b %b |  %b  %b |    %b", A, B, nA, nB, nAornB);
         A = 1; B = 1; #1
-        $display("%b %b |  %b  %b |    %b", A, B, nA, nB, nAandnB);
+        $display("%b %b |  %b  %b |    %b", A, B, nA, nB, nAornB);
     end
+
 endmodule
--- hw1.v
+++ hw1.v
@@ -1,16 +1,66 @@
+// ~A~B
+// module demorgan
+// (
+//     input A,    // first bit of input
+//     input B,    // second bit of input
+//     output nA,
+//     output nB,
+//     output nAandnB
+// );
+
+//     wire nA;
+//     wire nB;
+//     not Ainv(nA, A);
+//     not Binv(nB, B);
+//     and andgate(nAandnB, nA, nB);
+
+// endmodule
+
+
+// ~(A+B) 
+// module demorgan
+// (
+//     input A,    // first bit of input
+//     input B,    // second bit of input
+//     output AorB,
+//     output notAorB
+// );
+
+//     wire AorB;
+//     or AORB(AorB, A, B);
+//     not notAORB(notAorB, AorB);
+
+// endmodule
+
+// ~(AB) 
+// module demorgan
+// (
+//     input A,    // first bit of input
+//     input B,    // second bit of input
+//     output AandB,
+//     output notAandB
+// );
+
+//     wire AandB;
+//     and AandB(AandB, A, B);
+//     not notAANDB(notAandB, AandB);
+
+// endmodule
+
+// ~A+~B
 module demorgan
 (
     input A,    // first bit of input
     input B,    // second bit of input
     output nA,
     output nB,
-    output nAandnB
+    output nAornB
 );
 
     wire nA;
     wire nB;
     not Ainv(nA, A);
     not Binv(nB, B);
-    and andgate(nAandnB, nA, nB);
+    or orgate(nAornB, nA, nB);
 
 endmodule
